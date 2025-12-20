310e637c7096ce5bb0430edf624294b714b836c2
catis-99 <catisqsilva@gmail.com>
2025-12-20 17:26:23 +0000

    1

--- activio/src/app/home/home.page.ts
+++ activio/src/app/home/home.page.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonLabel,
+  IonButtons,
+  IonButton
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  notificationsOutline,
+  bicycleOutline,
+  waterOutline,
+  barbellOutline,
+  walkOutline,
+  timeOutline,
+  fitnessOutline,
+  flameOutline,
+  home,
+  list,
+  addCircle,
+  statsChart,
+  person
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonIcon,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonLabel,
+    IonButtons,
+    IonButton
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'notifications-outline': notificationsOutline,
+      'bicycle-outline': bicycleOutline,
+      'water-outline': waterOutline,
+      'barbell-outline': barbellOutline,
+      'walk-outline': walkOutline,
+      'time-outline': timeOutline,
+      'fitness-outline': fitnessOutline,
+      'flame-outline': flameOutline,
+      'home': home,
+      'list': list,
+      'add-circle': addCircle,
+      'stats-chart': statsChart,
+      'person': person
+    });
+  }
 }
